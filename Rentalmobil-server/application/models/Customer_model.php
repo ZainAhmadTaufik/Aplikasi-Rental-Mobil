c7986a53bf110a789445fb5e9ca7f87c2886fb41
ZAIN AHMAD TAUFIK <62564112+ZainAhmadTaufik@users.noreply.github.com>
2020-04-30 00:51:00 +0700

    Create Customer_model.php

---
+++ Rentalmobil-server/application/models/Customer_model.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Customer_model extends CI_Model
+{
+    public function getCustomer($id_customer = null)
+    {
+        if( $id_customer === null){
+            return $this->db->get('customer')->result_array();
+        }else{
+            return $this->db->get_where('customer', ['id_customer' => $id_customer])->result_array();
+        }
+    }
+
+    public function deleteCustomer($id_customer)
+    {
+        $this->db->delete('customer', ['id_customer' => $id_customer]);
+        return $this->db->affected_rows();
+    }
+
+    public function createCustomer($data)
+    {
+        $this->db->insert('customer', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function updateCustomer($data, $id_customer)
+    {
+        $this->db->update('customer', $data , ['id_customer' => $id_customer]);
+        return $this->db->affected_rows();
+    }
+}
+?>
