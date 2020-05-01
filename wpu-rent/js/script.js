a0945954158be496abfd1ac9f5b4aa069da7f5d2
donigagahhh <62916145+donigagahhh@users.noreply.github.com>
2020-05-01 17:23:38 +0700

    Add files via upload

---
+++ wpu-rent/js/script.js
@@ -0,0 +1,6 @@
+$.getJSON ('data/mobil.json' , function (data){
+	let menu = data.menu;
+	$.each(menu, function (i, data){
+		$('#daftar-mobil'). append('<div class="col-md-4"><div class="card mb-4"><img src="img/<?= $row["gambar"]; ?>" class="card-img-top"><div class="card-body"><h5 class="card-title"><?= $row["nama"]; ?></h5><p class="card-text"><?= $row["deskripsi"]; ?></p><h5 class="card-title"><?= $row["harga"]; ?></h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>')
+	});
+});
