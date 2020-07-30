f69cd667a7690886b2d877b06b9b0ad7e5bc66c7
ZAIN AHMAD TAUFIK <62564112+ZainAhmadTaufik@users.noreply.github.com>
2020-07-30 11:40:57 +0700

    Add files via upload

---
+++ Rencar-Appmvc/application/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col">
+        <center><img src="assets/images/b1.png"></center>
+        <center>Berkendara untuk menjelajahi setiap sudut kota, akan lebih efisien dengan menggunakan mobil. Anda dapat mempercayakan kepada kami, tak perlu bingung lagi karena anda dapat melakukan pemesanan rental mobil di website rentalmobil.com. Anda akan mendapatkan beragam pilihan kendaraan berkualitas dengan harga yang murah</center>
+        <center><img src="assets/images/Logo1.png" width="500px"></center>
+        </div>
+    </div>
+</div>
