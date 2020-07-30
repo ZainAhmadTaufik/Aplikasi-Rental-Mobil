0ad2434aaae2fdf612c93101c07d3542992823cc
ZAIN AHMAD TAUFIK <62564112+ZainAhmadTaufik@users.noreply.github.com>
2020-07-30 11:34:52 +0700

    Add files via upload

---
+++ Rencar-Appmvc/application/views/about_us/index.php
@@ -0,0 +1,15 @@
+
+<div class="container">
+    <div class="row mt-3">
+        <div class="col">
+        <img src="assets/images/Logo1.png"width="100">
+            <h1>Rental Mobil Boyz</h1>
+            <p>kami adalah jasa penyedia transportasi di yogyakarta, yang didukung dengan sopir pengalaman dengan jenis mobil mpv dan mobil harian yang dapat digunakan untuk wisata, dinas, maupun untuk acara-acara resmi.</p>
+            <br>
+            <p>Address:</p>
+            <p>jl. Prof.DR.Soepomo Sh, Warungboto, Kec. Umbulharjo, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55164</p>
+
+            <p>Phone : 0284-748 9485<br>Email : rentalmobil@gmail.com</p>        
+        </div>
+    </div>
+</div>
